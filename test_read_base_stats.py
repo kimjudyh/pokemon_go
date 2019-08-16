ca0ef6dbf01494e865419418eaa0208f84515384
Judy Kim <kimjudyh@gmail.com>
2019-08-15 18:39:36 -0700

    modify travis config to test only read_base_stats

--- test_read_base_stats.py
+++ test_read_base_stats.py
@@ -5,7 +5,6 @@ from multi_poke_v1 import read_base_stats
 def test_meltan():
     base_stats = read_base_stats("meltan")
 
-    assert base_stats[0] == "meltan"
-    assert base_stats[1] == 130
-    assert base_stats[2] == 118
-    assert base_stats[3] == 99
+    assert base_stats[0] == 130
+    assert base_stats[1] == 118
+    assert base_stats[2] == 99
