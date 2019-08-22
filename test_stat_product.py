1c5d7f5b060e4148d249aae3b349e2dd2a84123b
Judy Kim <kimjudyh@gmail.com>
2019-08-22 16:42:29 -0700

    add test file for pvp stats

---
+++ test_stat_product.py
@@ -0,0 +1,82 @@
+from stat_product import create_table, calc_stat_product, get_stat_product
+import math as m
+
+def test_azumarill():
+    try:
+        create_table("azumarill")
+        calc_stat_product("azumarill")
+    except Exception as e:
+        print("table exists")
+    PVP_stats = get_stat_product("azumarill", [8, 15, 15])
+
+    assert PVP_stats[0] == 1
+    assert round(PVP_stats[1]) == 2365612
+    assert round(PVP_stats[2]) == 100
+
+
+def test_skarmory():
+    try:
+        create_table("skarmory")
+        calc_stat_product("skarmory")
+    except Exception as e:
+        print("table exists")
+    PVP_stats = get_stat_product("skarmory", [0, 15, 14])
+
+    assert PVP_stats[0] == 1
+    assert round(PVP_stats[1]) == 2153020
+    assert round(PVP_stats[2]) == 100
+
+
+
+def test_medicham():
+    try:
+        create_table("medicham")
+        calc_stat_product("medicham")
+    except Exception as e:
+        print("table exists")
+    PVP_stats = get_stat_product("medicham", [15, 15, 15])
+
+    assert PVP_stats[0] == 1
+    assert round(PVP_stats[1]) == 1900834
+    assert round(PVP_stats[2]) == 100
+
+
+
+def test_lanturn():
+    try:
+        create_table("lanturn")
+        calc_stat_product("lanturn")
+    except Exception as e:
+        print("table exists")
+    PVP_stats = get_stat_product("lanturn", [0, 13, 14])
+
+    assert PVP_stats[0] == 1
+    assert round(PVP_stats[1]) == 2177678
+    assert round(PVP_stats[2]) == 100
+
+
+def test_bronzong():
+    try:
+        create_table("bronzong")
+        calc_stat_product("bronzong")
+    except Exception as e:
+        print("table exists")
+    PVP_stats = get_stat_product("bronzong", [1, 15, 14])
+
+    assert PVP_stats[0] == 1
+    assert round(PVP_stats[1]) == 2050591
+    assert round(PVP_stats[2]) == 100
+
+def test_altaria():
+    try:
+        create_table("altaria")
+        calc_stat_product("altaria")
+    except Exception as e:
+        print("table exists")
+    PVP_stats = get_stat_product("altaria", [0,14,15])
+
+    assert PVP_stats[0] == 1
+    assert round(PVP_stats[1]) == 2212160
+    assert round(PVP_stats[2]) == 100
+
+
