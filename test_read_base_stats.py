47f2757be6e0ed29183087b6e5baf36f770b9ef2
Judy Kim <kimjudyh@gmail.com>
2019-08-22 16:44:25 -0700

    add pvp stat test to travis file

--- test_read_base_stats.py
+++ test_read_base_stats.py
@@ -8,3 +8,14 @@ def test_meltan():
     assert base_stats[0] == 130
     assert base_stats[1] == 118
     assert base_stats[2] == 99
+
+def test_skarmory():
+    base_stats = read_base_stats("skarmory")
+    
+    assert base_stats[0] == 163
+    assert base_stats[1] == 148
+    assert base_stats[2] == 226
+
+
+
+    
