bd5106cbad34a454cab24d7e14913774d9709c09
Judy Kim <kimjudyh@gmail.com>
2019-10-05 00:16:26 -0700

    update test_read_file with csv files

--- test_file_read.py
+++ test_file_read.py
@@ -3,32 +3,33 @@
 from multi_poke_v1 import read_stats
 
 def test_read_stats_1():
-    envelope = read_stats("poke_data_1.txt")
-    assert len(envelope) == 8
-    assert type(envelope[0][2]) is int
-    assert type(envelope[0][3]) is int
-    assert type(envelope[0][4]) is int
+    pokemon_list = read_stats("./input_files/poke_data_1.csv")
+    assert len(pokemon_list) == 6
+    assert type(pokemon_list[0][2]) is int
+    assert type(pokemon_list[0][3]) is int
+    assert type(pokemon_list[0][4]) is int
+    assert type(pokemon_list[0][5]) is int
     
 def test_read_stats_2():
-    envelope = read_stats("poke_data_2.txt")
-    assert len(envelope) == 3
-    assert type(envelope[0][2]) is int
-    assert type(envelope[0][3]) is int
-    assert type(envelope[0][4]) is int
+    pokemon_list = read_stats("./input_files/poke_data_2.csv")
+    assert len(pokemon_list) == 20
+    assert type(pokemon_list[0][2]) is int
+    assert type(pokemon_list[0][3]) is int
+    assert type(pokemon_list[0][4]) is int
  
 def test_read_stats_3():
-    envelope = read_stats("poke_data_3.txt")
-    assert len(envelope) == 13
-    assert type(envelope[0][2]) is int
-    assert type(envelope[0][3]) is int
-    assert type(envelope[0][4]) is int
+    pokemon_list = read_stats("./input_files/poke_data_3.csv")
+    assert len(pokemon_list) == 9
+    assert type(pokemon_list[0][2]) is int
+    assert type(pokemon_list[0][3]) is int
+    assert type(pokemon_list[0][4]) is int
  
 def test_read_stats_4():
-    envelope = read_stats("poke_data_4.txt")
-    assert len(envelope) == 9
-    assert type(envelope[0][2]) is int
-    assert type(envelope[0][3]) is int
-    assert type(envelope[0][4]) is int
+    pokemon_list = read_stats("./input_files/poke_data_4.csv")
+    assert len(pokemon_list) == 11
+    assert type(pokemon_list[0][2]) is int
+    assert type(pokemon_list[0][3]) is int
+    assert type(pokemon_list[0][4]) is int
  
 #if __name__ == "__main__":
 #    test_read_stats()
